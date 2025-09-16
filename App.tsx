135d09b7e8cea20ba8a3fd13fe3679ae469b9835
souzannatha <souzanatha04@gmail.com>
2025-09-16 11:36:15 -0300

    feat: add custom fonts.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Jakarta-Sans-Bold': require('./assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'Jakarta-Sans-Regular': require('./assets/fonts/PlusJakartaSans-Regular.ttf'),
+  });
+  useEffect(() => {
+    async function hideSplash() {
+      if (fontsLoaded) {
+        await SplashScreen.hideAsync();
+      }
+    }
+    hideSplash();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Jakarta-Sans-Regular' }}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style='dark' />
     </View>
   );
