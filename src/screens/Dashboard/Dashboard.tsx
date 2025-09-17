728091f69d6ff041a533cf69224fc8a202595317
souzannatha <souzanatha04@gmail.com>
2025-09-17 08:19:50 -0300

    feat: add course card with title, buttons and description

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,11 +1,41 @@
-import { Image, View } from 'react-native';
+import { ImageBackground, TouchableOpacity, View, ScrollView } from 'react-native';
 import { styles } from './styles';
 import { Menu } from '../../components/Menu/Menu';
+import { Text } from '../../components/Text/Text';
+import { AntDesign, Entypo } from '@expo/vector-icons';
 
 export function Dashboard() {
   return (
-    <View>
+    <ScrollView>
       <Menu />
-    </View>
+      <ImageBackground
+        style={styles.cover}
+        source={require('../../../assets/images/women-cover.png')}
+      >
+        <View style={styles.titleContainer}>
+          <Text bold preset='heading' style={styles.title}>
+            Nome do Curso Lorem Ipsum br
+          </Text>
+          <TouchableOpacity style={styles.button}>
+            <View style={styles.buttonContent}>
+              <Entypo name='controller-play' size={20} color='black' />
+              <Text bold>Assistir</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonExclamation}>
+            <View style={styles.buttonContent}>
+              <AntDesign name='exclamation-circle' size={20} color='white' />
+              <Text bold style={styles.textExclamation}>
+                Mais informações
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <Text regular preset='paragraphSmall' style={styles.subtitle}>
+            Descrição bem curtinha lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore.
+          </Text>
+        </View>
+      </ImageBackground>
+    </ScrollView>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,4 +1,48 @@
 import { StyleSheet } from 'react-native';
 import { colors } from '../../styles/colors';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  cover: {
+    width: 400,
+    height: 400,
+  },
+  titleContainer: {
+    marginLeft: 16,
+    marginTop: 66,
+  },
+  title: {
+    color: colors.white,
+    width: 256,
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: colors.white,
+    width: 108,
+    height: 40,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  buttonExclamation: {
+    width: 194,
+    height: 40,
+    borderRadius: 4,
+    backgroundColor: 'rgba(255,255,255,0.3)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 24,
+  },
+  textExclamation: {
+    color: colors.white,
+  },
+  buttonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  subtitle: {
+    color: colors.white,
+    width: 256,
+  },
+});
